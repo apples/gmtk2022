a1e15adc05a60682d15cf4a0322180f957ef7f41
Apples <dbralir@gmail.com>
2022-07-16 05:17:34 -0500

    Improved lighting

---
+++ Assets/Scripts/Components/SpriteShadow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteShadow : MonoBehaviour
+{
+    public UnityEngine.Rendering.ShadowCastingMode mode;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnEnable()
+    {
+        spriteRenderer.shadowCastingMode = mode;
+    }
+
+    void OnDisable()
+    {
+        spriteRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+    }
+
+
+    void OnValidate()
+    {
+        spriteRenderer.shadowCastingMode = mode;
+    }
+}
